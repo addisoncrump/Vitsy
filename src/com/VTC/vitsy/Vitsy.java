ef5dbdd6bcadf59c0f05b2d662e4b31bb317e5eb
VTCAKAVSMoACE <dontemailme@example.com>
2015-11-05 15:44:58 +0000

    Fixed major issue with stack/instruction usage, spaced out lines for readability.

--- src/com/VTC/vitsy/Vitsy.java
+++ src/com/VTC/vitsy/Vitsy.java
@@ -3,6 +3,7 @@ import java.io.*;
 import java.util.*;
 
 public class Vitsy {
+	
 	private static boolean direction = true;
 	private static int position = 0;
 	@SuppressWarnings("all")
@@ -17,6 +18,7 @@ public class Vitsy {
 	private static Double tempvar = null;
 	private static Double globalvar = null;
 	private static boolean looping = false;
+	
 	@SuppressWarnings("all")
 	public static void main(String[] args) throws InterruptedException, IOException {
 		stac.add(new ArrayList(0));
@@ -53,22 +55,25 @@ public class Vitsy {
 			position = (direction) ? (position + 1): (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1;
 		}
 	}
+	
 	public static void methodHandler() throws InterruptedException, IOException {
-		for(;;) {
+		while (position < instruct.get(currinstruct).length) {
 			if (!looping && position > instruct.get(currinstruct).length - 1) {
-				currinstruct = oldpos.get(oldpos.size()-1)[0].intValue();
-				position = oldpos.get(oldpos.size()-1)[1].intValue();
-				oldpos.remove(oldpos.size()-1);
 				break;
 			}
 			else if (!instruct.get(currinstruct)[position].equals(""))
 				opHandle();
 			position = (direction) ? (position + 1): (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1;
 		}
+		currinstruct = oldpos.get(oldpos.size()-1)[0].intValue();
+		position = oldpos.get(oldpos.size()-1)[1].intValue();
+		oldpos.remove(oldpos.size()-1);
 	}
+	
 	public static void forLoopHandler(int reps) throws InterruptedException, IOException {
 		looping = true;
 		position = (direction) ? (position + 1)%instruct.get(currinstruct).length: (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1;
+		
 		if (!instruct.get(currinstruct)[position].equals("[")) {
 			for (int x = 0; x < reps; x++) {
 				opHandle();
@@ -85,15 +90,18 @@ public class Vitsy {
 		}
 		looping = false;
 	}
+	
 	public static void loopHandler() throws InterruptedException, IOException {
 		looping = true;
 		int startPos = position;
+		
 		while (true) {
 			position = (direction) ? (position + 1)%instruct.get(currinstruct).length: (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1;
 			if (instruct.get(currinstruct)[position].equals("]")) {
 				if (stac.get(currstac).get(stac.get(currstac).size()-1).intValue() == 0) { 
 					stac.get(currstac).remove(stac.get(currstac).size()-1);
 					break;
+
 				}
 				position = startPos+1;
 			}
@@ -101,57 +109,74 @@ public class Vitsy {
 		}
 		looping = false;
 	}
+	
 	@SuppressWarnings("all")
 	public static void opHandle() throws InterruptedException, IOException {
 		switch (OperativeHandler.doOperation((String) instruct.get(currinstruct)[position])) {
 		case "1":
 			stac.get(currstac).add(new Double(1));
 			break;
+
 		case "2":
 			stac.get(currstac).add(new Double(2));
 			break;
+
 		case "3":
 			stac.get(currstac).add(new Double(3));
 			break;
+
 		case "4":
 			stac.get(currstac).add(new Double(4));
 			break;
+
 		case "5":
 			stac.get(currstac).add(new Double(5));
 			break;
+
 		case "6":
 			stac.get(currstac).add(new Double(6));
 			break;
+
 		case "7":
 			stac.get(currstac).add(new Double(7));
 			break;
+
 		case "8":
 			stac.get(currstac).add(new Double(8));
 			break;
+
 		case "9":
 			stac.get(currstac).add(new Double(9));
 			break;
+
 		case "a":
 			stac.get(currstac).add(new Double(10));
 			break;
+
 		case "b":
 			stac.get(currstac).add(new Double(11));
 			break;
+
 		case "c":
 			stac.get(currstac).add(new Double(12));
 			break;
+
 		case "d":
 			stac.get(currstac).add(new Double(13));
 			break;
+
 		case "e":
 			stac.get(currstac).add(new Double(14));
 			break;
+
 		case "f":
 			stac.get(currstac).add(new Double(15));
 			break;
+
 		case "0":
 			stac.get(currstac).add(new Double(0));
 			break;
+
 		case "input":
 			if (input.size() == 0) stac.get(currstac).add((double) -1);
 			else {
@@ -159,19 +184,24 @@ public class Vitsy {
 				input.remove(input.size()-1);
 			}
 			break;
+
 		case "inlength":
 			stac.get(currstac).add((double)input.size());
 			break;
+
 		case "wait":
 			Thread.sleep((long) (stac.get(currstac).get(stac.get(currstac).size()-1)*1000));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "method":
-			oldpos.add(new Integer[]{currinstruct,(direction) ? (position + 1): (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1});
+			oldpos.add(new Integer[]{currinstruct,position});
 			currinstruct = stac.get(currstac).get(stac.get(currstac).size()-1).intValue();
 			position = 0;
+			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			methodHandler();
 			break;
+
 		case "file":
 			try {
 				String filename = "";
@@ -190,6 +220,7 @@ public class Vitsy {
 				}
 			} catch (FileNotFoundException e) {}
 			break;
+
 		case "ifnot":
 			if (stac.get(currstac).get(stac.get(currstac).size()-1).intValue() == 0 && instruct.get(currinstruct)[(direction) ? (position + 1)%instruct.get(currinstruct).length: (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1].equals("[")) {
 				stac.get(currstac).remove(stac.get(currstac).size()-1);
@@ -210,6 +241,7 @@ public class Vitsy {
 				}
 			} else stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "if":
 			if (!(stac.get(currstac).get(stac.get(currstac).size()-1).intValue() == 0) && instruct.get(currinstruct)[(direction) ? (position + 1)%instruct.get(currinstruct).length: (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1].equals("[")) {
 				stac.get(currstac).remove(stac.get(currstac).size()-1);
@@ -230,48 +262,62 @@ public class Vitsy {
 				}
 			} else stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "skip":
 			position = (direction) ? (position + 1)%instruct.get(currinstruct).length: (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1;
 			break;
+
 		case "loop":
 			loopHandler();
 			break;
+
 		case "sine":
 			stac.get(currstac).set(stac.get(currstac).size()-1,(Math.sin(stac.get(currstac).get(stac.get(currstac).size()-1))));
 			break;
+
 		case "asine":
 			stac.get(currstac).set(stac.get(currstac).size()-1,(Math.asin(stac.get(currstac).get(stac.get(currstac).size()-1))));
 			break;
+
 		case "cosine":
 			stac.get(currstac).set(stac.get(currstac).size()-1,(Math.cos(stac.get(currstac).get(stac.get(currstac).size()-1))));
 			break;
+
 		case "acosine":
 			stac.get(currstac).set(stac.get(currstac).size()-1,(Math.acos(stac.get(currstac).get(stac.get(currstac).size()-1))));
 			break;
+
 		case "tangent":
 			stac.get(currstac).set(stac.get(currstac).size()-1,(Math.tan(stac.get(currstac).get(stac.get(currstac).size()-1))));
 			break;
+
 		case "atangent":
 			stac.get(currstac).set(stac.get(currstac).size()-1,(Math.atan(stac.get(currstac).get(stac.get(currstac).size()-1))));
 			break;
+
 		case "repnextchar":
 			int repeats = stac.get(currstac).get(stac.get(currstac).size()-1).intValue();
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			forLoopHandler(repeats);
 			break;
+
 		case "length":
 			stac.get(currstac).add((double)stac.get(currstac).size());
 			break;
+
 		case "pi":
 			stac.get(currstac).add((Math.PI));
 			break;
+
 		case "E":
 			stac.get(currstac).add((Math.E));
 			break;
+
 		case "log":
 			stac.get(currstac).set(stac.get(currstac).size()-2, (Math.log(stac.get(currstac).get(stac.get(currstac).size()-2))/Math.log(stac.get(currstac).get(stac.get(currstac).size()-1))));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "outnum":
 			Double x = stac.get(currstac).get(stac.get(currstac).size()-1);
 			if (x > 10000 && x.intValue() == x)
@@ -284,13 +330,16 @@ public class Vitsy {
 				System.out.print(x);
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "rand":
 			stac.get(currstac).set(stac.get(currstac).size()-1, (Math.random()*stac.get(currstac).get(stac.get(currstac).size()-1)));
 			break;
+
 		case "outchar":
 			System.out.print((char) stac.get(currstac).get(stac.get(currstac).size()-1).intValue());
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "end":
 			System.exit(0);
 		case "printall":
@@ -300,6 +349,7 @@ public class Vitsy {
 				stac.get(currstac).remove(stac.get(currstac).size()-1);
 			}
 			break;
+
 		case "getall":
 			int reps2 = input.size();
 			for(int i = reps2 - 1; i >= 0; i--) {
@@ -307,13 +357,16 @@ public class Vitsy {
 				input.remove(input.size()-1);
 			}
 			break;
+
 		case "teleport":
 			position = stac.get(currstac).get(stac.get(currstac).size()-1).intValue()-2;
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "reverse":
 			Collections.reverse(stac.get(currstac));
 			break;
+
 		case "rotateright":
 			@SuppressWarnings("all")
 			ArrayList<Double> temp1 = new ArrayList(0);
@@ -323,6 +376,7 @@ public class Vitsy {
 			stac.remove(currstac);
 			stac.add(temp1);
 			break;
+
 		case "rotateleft":
 			@SuppressWarnings("all")
 			ArrayList<Double> temp2 = new ArrayList(0);
@@ -332,9 +386,11 @@ public class Vitsy {
 			stac.remove(currstac);
 			stac.add(temp2);
 			break;
+
 		case "duplicate":
 			stac.get(currstac).add(stac.get(currstac).get(stac.get(currstac).size()-1));
 			break;
+
 		case "tempvar":
 			if (tempvar == null) {
 				tempvar = stac.get(currstac).get(stac.get(currstac).size()-1);
@@ -345,6 +401,7 @@ public class Vitsy {
 				tempvar = null;
 			}
 			break;
+
 		case "globalvar":
 			if (globalvar == null) {
 				globalvar = stac.get(currstac).get(stac.get(currstac).size()-1);
@@ -352,57 +409,72 @@ public class Vitsy {
 			}
 			else stac.get(currstac).add(globalvar);
 			break;
+
 		case "remove":
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "part":
 			stac.get(currstac).set(stac.get(currstac).size()-1, stac.get(currstac).get(stac.get(currstac).get(stac.get(currstac).size()-1).intValue()));
 			break;
+
 		case "changestack":
 			currstac = (currstac + 1) % stac.size();
 			break;
+
 		case "clnstack": 
 			stac.add(stac.get(currstac));
 			currstac = (currstac + 1) % stac.size();
 			break;
+
 		case "newstack": 
 			stac.add(new ArrayList(0));
 			currstac = (currstac + 1) % stac.size();
 			break;
+
 		case "rmstack":
 			stac.remove(currstac);
 			break;
+
 		case "numstack":
 			stac.get(currstac).add((double) stac.size());
 			break;
+
 		case "add":
 			stac.get(currstac).set(stac.get(currstac).size()-2,stac.get(currstac).get(stac.get(currstac).size()-1)+(stac.get(currstac).get(stac.get(currstac).size()-2)));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "subtract":
 			stac.get(currstac).set(stac.get(currstac).size()-2,stac.get(currstac).get(stac.get(currstac).size()-2)-(stac.get(currstac).get(stac.get(currstac).size()-1)));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "multiply":
 			stac.get(currstac).set(stac.get(currstac).size()-2,stac.get(currstac).get(stac.get(currstac).size()-1)*(stac.get(currstac).get(stac.get(currstac).size()-2)));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "divide":
 			stac.get(currstac).set(stac.get(currstac).size()-2,stac.get(currstac).get(stac.get(currstac).size()-2)/(stac.get(currstac).get(stac.get(currstac).size()-1)));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "equal":
 			stac.get(currstac).set(stac.get(currstac).size()-2, (double) ((stac.get(currstac).get(stac.get(currstac).size()-2).equals(stac.get(currstac).get(stac.get(currstac).size()-1)))? 0: 1));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "modulo":
 			stac.get(currstac).set(stac.get(currstac).size()-2,stac.get(currstac).get(stac.get(currstac).size()-2)%stac.get(currstac).get(stac.get(currstac).size()-1));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "power":
 			stac.get(currstac).set(stac.get(currstac).size()-2,Math.pow(stac.get(currstac).get(stac.get(currstac).size()-2),stac.get(currstac).get(stac.get(currstac).size()-1)));
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
 			break;
+
 		case "factorial":
 			double output = 1;
 			Double y = stac.get(currstac).get(stac.get(currstac).size()-1);
@@ -411,13 +483,16 @@ public class Vitsy {
 			}
 			stac.get(currstac).set(stac.get(currstac).size()-1, output);
 			break;
+
 		case "quote":
 			while (true) {
 				position = (direction) ? (position + 1)%instruct.get(currinstruct).length: (position - 1 >= 0) ? position - 1: instruct.get(currinstruct).length-1;
 				if (instruct.get(currinstruct)[position].equals("\"") || instruct.get(currinstruct)[position].equals("\'")) break;
+
 				stac.get(currstac).add(((double)((String)instruct.get(currinstruct)[position]).toCharArray()[0]));
 			}
 			break;
+
 		case "prime":
 			Double n = stac.get(currstac).get(stac.get(currstac).size()-1);
 			stac.get(currstac).remove(stac.get(currstac).size()-1);
@@ -431,20 +506,26 @@ public class Vitsy {
 			}
 			stac.get(currstac).add(new Double(outputp));
 			break;
+
 		case "changedir":
 			direction = !direction;
 			break;
+
 		case "goleft":
 			direction = false;
 			break;
+
 		case "goright":
 			direction = true;
 			break;
+
 		case "randdir":
 			direction = (Math.random()<.5);
 			break;
+
 		case "nothing":
 			break;
+
 		}
 	}
 }
