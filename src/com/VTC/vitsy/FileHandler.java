19531cde523da63ebfc10238b04e26a21d5388f0
VTCAKAVSMoACE <dontemailme@example.com>
2015-11-20 08:38:32 +0000

    Nothing to see here, move along...

--- src/com/VTC/vitsy/FileHandler.java
+++ src/com/VTC/vitsy/FileHandler.java
@@ -20,8 +20,11 @@ public class FileHandler {
 			file.add(";".split(""));
 		}
 		if (file.size() == 0) {
-			for (int i = 99; i > 2; i--)
-				System.out.println(i+" bottles of beer on the wall, "+i+" bottles of beer.\nTake one down and pass it around, "+i+" bottles of beer.");
+			for (int i = 99; i > 2;) {
+				System.out.print(i+" bottles of beer on the wall, "+i+" bottles of beer.\nTake one down and pass it around, ");
+				i--;
+				System.out.println(i+" bottles of beer.");
+			}
 			System.out.println("2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer.");
 			System.out.println("1 bottle of beer on the wall, 1 bottle of beer.\nGo to the store and buy some more, 99 bottles of beer.");
 			System.exit(0);
